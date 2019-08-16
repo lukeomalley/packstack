a4469c1e6c8b0aa174766b06a03b0b2c447870ab
Luke O'Malley <Lukeomalley95@gmail.com>
2019-08-16 09:47:03 -0400

    defined index route

--- app/controllers/packs_controller.rb
+++ app/controllers/packs_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class PacksController < ApplicationController
+  def index
+    render json: Pack.all
+  end
+
+  def show; end
+
+  def create; end
+
+  def update; end
+
+  def destroy; end
 end
