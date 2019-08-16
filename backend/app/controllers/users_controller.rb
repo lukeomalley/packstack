65d29e8a99d8d45899e3d295722bbdf4324f461e
lesliesage <leslie.sage@gmail.com>
2019-08-16 11:36:20 -0400

    routes and user controller done

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,4 +1,37 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+    def show
+        render json: User.find(params[:id])
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save!
+            render json: user
+        else
+            render json: {error: 'You failed.'}
+        end
+    end
+
+    def update
+        user = User.find(:id)
+        if user.update(user_params)
+            render json: user
+        else
+            render json: {error: 'You failed.'}
+        end
+    end
+
+    def destroy
+        user = User.find(:id)
+        user.destroy
+        render json: User.all
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:id, :name, :image_url)
+    end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # TODO: remove the routes that we do not need
-  resources :users
-  resources :views
-  resources :cards
-  resources :packs
+  resources :users, only: [:show, :create, :update, :destroy]
+  resources :cards, only: [:create, :update, :destroy]
+  resources :packs, only: [:index, :show, :create, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
