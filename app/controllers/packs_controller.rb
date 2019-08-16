1efbe919542a79c00d3ac95146b921da924628c0
Luke O'Malley <Lukeomalley95@gmail.com>
2019-08-16 09:50:47 -0400

    implemented create action and user params private method

--- app/controllers/packs_controller.rb
+++ app/controllers/packs_controller.rb
@@ -9,9 +9,23 @@ class PacksController < ApplicationController
     render json: Pack.find(params[:id])
   end
 
-  def create; end
+  def create
+    user = User.new(user_params)
+    if user.save!
+      render json: user
+    else
+      render json: {error: 'try again next time pal'}
+    end
+  end
 
   def update; end
 
   def destroy; end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :image_url)
+  end
+
 end
