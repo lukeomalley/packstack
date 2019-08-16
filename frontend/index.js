238621191451c9cbdfdd40df2f9d932faaca6a12
Luke O'Malley <Lukeomalley95@gmail.com>
2019-08-16 12:48:07 -0400

    pack card styles

--- frontend/index.js
+++ frontend/index.js
@@ -1,26 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("yo.")
-    fetchPacks()
-})
-function fetchPacks(){
-    fetch("http://localhost:3000/packs")
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('yo.');
+  fetchPacks();
+});
+function fetchPacks() {
+  fetch('http://localhost:3000/packs')
     .then(resp => resp.json())
-    .then(packsArray => renderPacks(packsArray))
+    .then(packsArray => renderPacks(packsArray));
 }
-function renderPacks(packsArray){
-    packsArray.forEach(renderPack)
+function renderPacks(packsArray) {
+  packsArray.forEach(renderPack);
 }
-function renderPack(pack){
-    const packsDiv = document.getElementById("packs-container")
-    const packDiv = document.createElement("div")
-
-    packDiv.innerHTML = `
+function renderPack(pack) {
+  const packsDiv = document.getElementById('packs-container');
+  const packDiv = document.createElement('div');
+  packDiv.classList.add('pack-card');
+  packDiv.innerHTML = `
         <img src=${pack.image_url}>
         <div>
             <h2>${pack.name}</h2>
             <h3>${pack.category}</h3>
             <h3>${pack.user.name}</h3>
         </div>
-        `
-    packsDiv.appendChild(packDiv)
+        `;
+  packsDiv.appendChild(packDiv);
 }
