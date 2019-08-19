470dc7ecaaa32528ed1917a36df9d8dd336514ac
lesliesage <leslie.sage@gmail.com>
2019-08-19 12:34:27 -0400

    pack show page formatted

--- frontend/src/pack.js
+++ frontend/src/pack.js
@@ -10,29 +10,35 @@ function fetchPack(id) {
 
 function renderPackDeets(pack) {
   const packPageConatiner = document.getElementById('pack-show-page');
-  console.log(pack);
   const createDate = new Date(pack.created_at).toLocaleDateString('en-US', {
     month: 'long',
     year: 'numeric',
   });
   packPageConatiner.innerHTML = `
   <div class="deets">
-  <img src='../../backend/app/assets/images/${pack.image_url}' alt='Picture of ${pack.name}'>
-    <h2>${pack.name}</h2>
-    <p>${pack.description}</p>
-    <p>Created: ${createDate} by ${pack.user.name}</p>
+    <div class="deets-card">
+      <img src='../../backend/app/assets/images/${pack.image_url}' alt='Picture of ${pack.name.toLowerCase()}' class='deets-image'>
+      <h2>${pack.name}</h2>
+    </div>
+    <div class="deets-preview">
+      <h2>${pack.description}</h2>
+      <p>Created: ${createDate} by ${pack.user.name}</p>
+      <form id="start-form">
+        <label>Set timer per flashcard (sec)</label>
+        <div id="start-stuff">
+          <input type="number" placeholder="10"/>
+          <button class="start-form-bttn">Start</button>
+        </div>
+      </form>
+      
+      <div class="play">
+      
+      </div>
+      
+      <div class="stats">
+      
+      </div>
+    </div>
   </div>
-  <div class="preview">
-    ${pack.cards[0] ? pack.cards[0].question : null}
-    <form id="start-form">
-      <label>Set timer per flashcard (sec)</label>
-      <input type="number" placeholder="10"/>
-      <button>🚗 Start</button>
-    </form>
-  </div>
-  <div class="play">
-
-  </div>
-  <div class="stats"></div>
   `;
 }
